fc7ca6e4971e183a840b8cb90f8edee54800b2fe
smmuja <smmuja27@gmail.com>
2024-12-11 22:02:10 +0700

    fix: sidebar navigation

--- src/layout/components/LeftSideBar.tsx
+++ src/layout/components/LeftSideBar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { CiHome } from "react-icons/ci";
 import { MdManageAccounts } from "react-icons/md";
 
@@ -6,7 +7,10 @@ export function LeftSideBar() {
     <>
       <div className="bg-white shadow-md w-fit h-screen mt-5">
         <div>
-          <CiHome className="m-5 size-10 bg-teal-500 rounded-md text-white" />
+          <Link href={"/"}>
+            <CiHome className="m-5 size-10 bg-teal-500 rounded-md text-white" />
+          </Link>
+
           <MdManageAccounts className="m-5 size-10 bg-transparent rounded-md text-teal-500" />
         </div>
       </div>
