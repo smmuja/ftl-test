d63f932848d4bcf473534a5bd263ef735760cc7e
smmuja <smmuja27@gmail.com>
2024-12-11 21:49:13 +0700

    style: fix navbar

--- src/layout/components/Navbar.tsx
+++ src/layout/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { FaUser } from "react-icons/fa";
 export function Navbar() {
   return (
     <>
-      <nav className="bg-gradient-to-r from-black to-teal-900 flex justify-between lg:justify-around items-center px-3 py-2 sticky top-0 text-green-tosca font-semibold z-10 h-[90px] w-screen p-3">
+      <nav className="bg-gradient-to-r from-black to-teal-900 flex justify-between items-center w-full sticky top-0 text-white font-semibold z-10 h-[90px] p-3">
         <Link href="/" className="text-white">
           FTL iMeeting
         </Link>
